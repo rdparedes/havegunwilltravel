d1926004f139a23a988b8b47ecfe7a2f55aacdf8
Roberto Paredes <rdparedessalazar@gmail.com>
2016-09-23 17:48:51 -0500

    setup react webpack environment

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import Component from 'react'
+
+class Header extends Component {
+  render () {
+    return (
+      <div className='App-header'>
+        <h2>Sample site</h2>
+      </div>
+    )
+  }
+}
+
+export default Header
