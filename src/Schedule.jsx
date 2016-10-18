30ce1a3c0d5d3d770206ef66b44ad09ca1893386
Roberto Paredes <rdparedessalazar@gmail.com>
2016-10-18 00:25:53 -0500

    update layout

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -12,14 +12,33 @@ export default class Schedule extends Component {
                 <div className='body'>
                     <div className="schedule">
                         <div className="schedule-header">
-                        
+                            <div className="row">
+                                <div className="col s2 m1">
+                                    <i className="medium material-icons">label_outline</i>
+                                </div>
+                                <h3 style={{paddingTop: '6px'}}>Lunes 31 - $400 - 57km</h3>
+                            </div>
                         </div>
-                        <div className="schedule-row">
-                            
-                        </div>
-                        <div className="schedule-row">
-                            
-                        </div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Horario</th>
+                                    <th>Evento</th>
+                                    <th>$</th>
+                                    <th>Kms.</th>
+                                    <th>URL</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>07:05 - 8:00</td>
+                                    <td>Vuelo UIO-CUE</td>
+                                    <td>100</td>
+                                    <td>0</td>
+                                    <td><a href="https://www.google.com" target="_blank">www.google.com</a></td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                     <div className="photos">
                         <div className="row">
@@ -33,6 +52,7 @@ export default class Schedule extends Component {
                             </div>
                         </div>
                         <img className="responsive-img" src="./images/demo_img1.jpg" />
+                        <img className="responsive-img" src="./images/demo_img2.jpg" />
                     </div>
                 </div>
             </div>
