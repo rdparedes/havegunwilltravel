233b7646e784a9a10278f980167c6cad79d4ea06
Roberto Paredes <rdparedessalazar@gmail.com>
2016-10-31 00:54:17 -0500

    added event add/remove feature

--- src/Day.jsx
+++ src/Day.jsx
@@ -23,13 +23,34 @@ export default class Day extends Component {
       state: 'events',
       asArray: true
     })
+    this.deleteEvent = (index) => {
+      let updatedEvents = this.state.events
+      updatedEvents.splice(index, 1)
+      this.setState({ events: updatedEvents })
+    }
   }
   addEvent () {
     $('#add-event-form' + this.props.date).show()
     this.setState({ addEventFormIsVisible: true })
   }
+  getEventValues () {
+    return {
+      schedule: $('#' + this.props.date + ' #horario').val(),
+      name: $('#' + this.props.date + ' #evento').val(),
+      cost: parseFloat($('#' + this.props.date + ' #costo').val() || 0),
+      carDistance: parseFloat($('#' + this.props.date + ' #kms').val() || 0),
+      description: $('#' + this.props.date + ' #cosas').val(),
+      url: $('#' + this.props.date + ' #url').val(),
+      key: this.state.events.length
+    }
+  }
   saveEvent () {
-    return
+    let event = this.getEventValues()
+    if (!event.name) {
+      return
+    }
+    this.setState({ events: this.state.events.concat(event) })
+    this.cancelAddEvent()
   }
   cancelAddEvent () {
     $('#add-event-form' + this.props.date).hide()
@@ -37,54 +58,61 @@ export default class Day extends Component {
   }
   render () {
     let dayEvents = []
-    if (this.state.events !== null && this.state.events !== undefined) {
+    let addEventSection
+    let saveEventSection
+    let addEventForm
+    if (this.state.events) {
       dayEvents = this.state.events.map((event, index) => (
-        <DayEvent key={index} carDistance={event.carDistance} cost={event.cost} name={event.name}
-          description={event.description} schedule={event.schedule} url={event.url} />
+        <DayEvent key={event.key} carDistance={event.carDistance} cost={event.cost} name={event.name}
+          description={event.description} schedule={event.schedule} url={event.url}
+          deleteEvent={this.deleteEvent} index={event.key} />
       ))
     }
-    let addEventSection = <div className='row event'>
-      <div className='col s12'>
-        <button id={'add-event' + this.props.date} className='waves-effect waves-light btn grey'
-          onClick={ () => this.addEvent() }>
-          + Evento
-        </button>
-      </div>
-    </div>
-    let saveEventSection = <div className='row event'>
-      <div className='col s12'>
-        <button id={ 'save-event' + this.props.date } className='waves-effect waves-light btn'
-          onClick={ () => this.saveEvent() }>
-          Guardar
-        </button>
-        <button id={ 'cancel-event' + this.props.date } className='waves-effect waves-light btn grey'
-          onClick={ () => this.cancelAddEvent() }>
-          Cancelar
-        </button>
-      </div>
-    </div>
-    let addEventForm = <form className='col s12'>
-      <div className='row'>
-        <div className='input-field col s12 m3'>
-          <input placeholder='Horario' id='horario' type='text' className='validate' />
-        </div>
-        <div className='input-field col s12 m7'>
-          <input placeholder='Evento' id='evento' type='text' className='validate' />
-        </div>
-        <div className='input-field col s12 m2'>
-          <input placeholder='Costo' id='costo' type='text' className='validate' />
-        </div>
-        <div className='input-field col s12 m2'>
-          <input placeholder='Kms' id='kms' type='text' className='validate' />
-        </div>
-        <div className='input-field col s12 m6'>
-          <input placeholder='Cosas' id='cosas' type='text' className='validate' />
-        </div>
-        <div className='input-field col s12 m4'>
-          <input placeholder='URL' id='url' type='text' className='validate' />
+    if (this.props.loggedIn) {
+      addEventSection = <div className='row event'>
+        <div className='col s12'>
+          <button id={'add-event' + this.props.date} className='waves-effect waves-light btn grey'
+            onClick={ () => this.addEvent() }>
+            + Evento
+          </button>
         </div>
       </div>
-    </form>
+      saveEventSection = <div className='row event'>
+        <div className='col s12'>
+          <button id={ 'save-event' + this.props.date } className='waves-effect waves-light btn'
+            onClick={ () => this.saveEvent() }>
+            Guardar
+          </button>
+          <button id={ 'cancel-event' + this.props.date } className='waves-effect waves-light btn grey'
+            onClick={ () => this.cancelAddEvent() }>
+            Cancelar
+          </button>
+        </div>
+      </div>
+      addEventForm = <form className='col s12'>
+        <div className='row'>
+          <div className='input-field col s12 m3'>
+            <input placeholder='Horario' id='horario' type='text' className='validate' />
+          </div>
+          <div className='input-field col s12 m7'>
+            <input placeholder='Evento (obligatorio)' id='evento' type='text' className='validate'
+              required/>
+          </div>
+          <div className='input-field col s12 m2'>
+            <input placeholder='Costo' id='costo' type='number' step='any' className='validate' />
+          </div>
+          <div className='input-field col s12 m2'>
+            <input placeholder='Kms' id='kms' type='number' step='any' className='validate' />
+          </div>
+          <div className='input-field col s12 m6'>
+            <input placeholder='Cosas' id='cosas' type='text' className='validate' />
+          </div>
+          <div className='input-field col s12 m4'>
+            <input placeholder='URL' id='url' type='text' className='validate' />
+          </div>
+        </div>
+      </form>
+    }
 
     return (
       <div id={this.props.date} className='day'>
@@ -102,11 +130,12 @@ export default class Day extends Component {
           <thead>
             <tr>
               <th width='12%'>Horario</th>
-              <th width='25%'>Evento</th>
+              <th width='23%'>Evento</th>
               <th width='8%'>$</th>
               <th width='8%'>Kms.</th>
-              <th width='32%'>Cosas</th>
+              <th width='29%'>Cosas</th>
               <th width='15%'>URL</th>
+              <th width='5%'></th>
             </tr>
           </thead>
           <tbody>
--- src/DayEvent.jsx
+++ src/DayEvent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { base } from './App.jsx'
 
 export default class DayEvent extends Component {
   render () {
@@ -6,10 +7,14 @@ export default class DayEvent extends Component {
       <tr>
         <td>{ this.props.schedule }</td>
         <td>{ this.props.name }</td>
-        <td>{ this.props.totalCost }</td>
+        <td>{ this.props.cost }</td>
         <td>{ this.props.carDistance }</td>
         <td>{ this.props.description }</td>
         <td><a href={this.props.url} target='_blank'>{ this.props.url }</a></td>
+        <td>
+          <button className='btn-flat waves-effect waves-light'
+            onClick={ () => this.props.deleteEvent(this.props.index) }>×</button>
+        </td>
       </tr>
     )
   }
